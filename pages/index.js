5142a51dd14a83fe2c83c5fdf4090300c765ef9f
GustavoAniel <aniel.amtimil@yahoo.com.br>
2022-12-16 18:04:30 -0300

    Primeiro Commit

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import HomeScreen from '../src/screens/HomeSreen';
+
+export default HomeScreen;
+
+// import React from "react";
+// import Link from "../src/components/Link";
+
+// function GlobalStyle(){
+//     return(
+//         <style global jsx>{`
+//             body {
+//                 font-family: sans-serif;
+//             }
+//         `}
+//         </style>
+//     )
+// }
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <>
+//             <Tag>
+//                 {children}
+//             </Tag>  
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+//             `}</style>
+//         </>
+//     );
+// }
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <GlobalStyle />
+//             <Title as="h1">Alura Cases - Home Page</Title>
+//             <Link href="/faq">Ir para a página do Faq</Link>
+//         </div>
+//     );
+// }
