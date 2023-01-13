5791cdaa50f53a0791c8c0ec1e0265d57b6a6458
GustavoAniel <aniel.amtimil@yahoo.com.br>
2023-01-13 16:37:57 -0300

    Ajusta faq e home

--- pages/faq.js
+++ pages/faq.js
@@ -1,35 +1,24 @@
-import { useEffect, useState } from "react";
-import Link from "../src/components/Link";
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
-export default function FAQPage() {
-    const [faq, setFaq] = useState([]);
-    useEffect(() => {
-        const FAQ_API_URL =
-            "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
-        fetch(FAQ_API_URL)
-            .then((respostaDoServidor) => {
-                return respostaDoServidor.json();
-            })
-            .then((resposta) => {
-                setFaq(resposta);
-            });
-    }, []);
+export default FAQScreen; 
 
-    return (
-        <div>
-            <h1>Alura Cases - Pagína de Perguntas</h1>
-            <Link href="/">Ir para o HOME</Link>
-            <ul>
-                {faq.map(({ answer, question }) => (
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
 
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+    return {
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
